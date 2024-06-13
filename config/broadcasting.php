a437d67fa920b269e4c5e1783607b89a03c6aeae
ChandraLim92 <seven7planet@yahoo.com>
2024-06-13 20:02:49 +0700

    test websocket

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,10 +37,10 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => false,
+                // 'encrypted' => true,
                 'host' => env('PUSHER_APP_HOST'), //'192.168.11.100'
                 'port' => env('LARAVEL_WEBSOCKETS_PORT'),
-                'scheme' => 'http',
+                'scheme' => 'https',
                 // 'useTLS' => true,
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
